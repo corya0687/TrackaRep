5e65e6426a7573065559981445824dd617bde9fe
corya0687 <corya0687@gmail.com>
2016-08-17 11:16:26 -0400

    added new row to home page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
   def index
-    @exercises = Exercise.paginate(page: params[:page], per_page: 4)
-    @workouts = Workout.paginate(page: params[:page], per_page: 4)
+    @exercises = Exercise.paginate(page: params[:page], per_page: 3)
+    @workouts = Workout.paginate(page: params[:page], per_page: 3)
     @plans = Plan.all
   end
 
