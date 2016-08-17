95ca5df17fe4cc105f39eaa3e09d14f8d4cdd651
corya0687 <corya0687@gmail.com>
2016-08-17 17:03:43 -0400

    finished jquery recent run loading on welcome page

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,3 +1,30 @@
+'use strict';
+
+var runs;
 $(function () {
-  $()
+  recentRuns();
+
 });
+
+
+
+function recentRuns() {
+    let user_id = $("#see-all-ran").attr("href").match(/\d+/)[0]
+    $.get('/users/'+user_id+'/runs.json',function (response) {
+      console.log(response)
+      runs = response;
+    }).done(function () {
+      addRunsToWelcome(runs);
+    });
+}
+
+function addRunsToWelcome(runs) {
+  runs.forEach(function (run) {
+    displayRun(run);
+  });
+}
+
+
+function displayRun(run) {
+  $("#recent-runs").append("<tr id="+run.id+"><td>"+run.exercise.name+"</td><td>"+run.exercise.sets+"</td><td></td><td>"+run.duration+"</td></tr>")
+}
