e7876d40e7d1e96012b584741dc61dec9ad434cd
corya0687 <corya0687@gmail.com>
2016-08-17 10:49:13 -0400

    added basic pundit authentications

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   include Pundit
   protect_from_forgery with: :exception
-  #before_action :authenticate_user!
+  before_action :authenticate_user!
 
   layout :layout_by_resource
 
