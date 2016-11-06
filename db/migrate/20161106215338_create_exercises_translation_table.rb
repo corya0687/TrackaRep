4b981a1031d4eebf89ba38326b4cfa3e9d8aeac2
corya0687 <corya0687@gmail.com>
2016-11-06 17:14:53 -0500

    updated exercise translation table migration to create a table

--- db/migrate/20161106215338_create_exercises_translation_table.rb
+++ db/migrate/20161106215338_create_exercises_translation_table.rb
@@ -1,6 +1,17 @@
 class CreateExercisesTranslationTable < ActiveRecord::Migration
   def change
-    create_table :exercises_translation_tables do |t|
+    create_table :exercises do |t|
+      t.timestamps
+    end
+
+    reversible do |dir|
+        dir.up do
+          Exercise.create_translation_table! :name => :string, :description => :string
+        end
+
+        dir.down do
+          Exercise.drop_translation_table!
+        end
     end
   end
 end
