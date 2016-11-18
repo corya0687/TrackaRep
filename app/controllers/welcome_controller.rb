9ac0b0a576b7cc60ac5de452865bb9369b020011
corya0687 <corya0687@gmail.com>
2016-11-18 18:25:28 -0500

    added home route, action skip authetication

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,14 @@
 class WelcomeController < ApplicationController
+ before_action :authenticate_user!, :only => [:index] 
+
   def index
     @exercises = Exercise.paginate(page: params[:page], per_page: 3)
     @workouts = Workout.paginate(page: params[:page], per_page: 3)
     @plans = Plan.all
+  end
+
+  def home
+
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -25,7 +25,7 @@ Rails.application.routes.draw do
 
    resources :exercises, only: [:index, :show]
 
-     root 'welcome#index'
+     root 'welcome#home'
   end
 
 
