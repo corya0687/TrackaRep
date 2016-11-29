3c6cc697f0c99cfdc0e0b1a5af881970bb998bd2
corya0687 <corya0687@gmail.com>
2016-11-28 21:45:02 -0500

    added

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -38,22 +38,19 @@ before_action :configure_account_update_params, only: [:update]
 
   protected
 
-  If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
     devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
   end
 
-  If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
     devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
   end
 
-  The path used after sign up.
   def after_sign_up_path_for(resource)
     super(resource)
   end
 
-  The path used after sign up for inactive accounts.
+  # The path used after sign up for inactive accounts.
   def after_inactive_sign_up_path_for(resource)
     super(resource)
   end
