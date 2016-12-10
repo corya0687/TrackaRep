009029dee14fcd8f6bac30af7cd808f6fd7a67b3
corya0687 <corya0687@gmail.com>
2016-12-10 15:17:44 -0500

    added workout name

--- app/assets/javascripts/runs.js
+++ app/assets/javascripts/runs.js
@@ -1,11 +1,14 @@
 $(document).ready(function () {
  attachRunListeners();
+ var wkHeader;
 });
+
 
 function attachRunListeners() {
   setRunDataUrl();
   editWorkout();
   showWorkoutHeader();
+
 }
 
 function setRunDataUrl() {
@@ -33,18 +36,22 @@ function updateRun(url, data) {
 function editWorkout() {
   $("#workout-run-input").hide();
   $("#save-workout-name").hide();
+  wkHeader = $("#workout-header").text();
   $("#edit-workout-name").on('click', function () {
     $("#workout-header").hide();
     $("#edit-workout-name").hide();
     $("#workout-run-input").show();
     $("#save-workout-name").show();
-
+    $("#workout-run-input").val(wkHeader)
   });
 }
 
 function showWorkoutHeader() {
   $("#save-workout-name").on('click', function () {
+    var wkInput = $("#workout-run-input").val();
+    wkHeader = wkInput;
     $("#workout-run-input").hide();
+    $("#workout-header").text(wkInput)
     $("#workout-header").show();
     $("#edit-workout-name").show();
     $("#save-workout-name").hide();
